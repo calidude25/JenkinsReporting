33d34bae35a0916e068f3f8b5dac68159020cb5c
carsm013 <matt.b.carson.-nd@disney.com>
2016-04-13 12:20:41 -0600

    S

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 BIN=$(cd -- "$(dirname "$0")" && pwd)
 pwd
-java -classpath target/jenkins-reporting.jar:$BIN com.disney.wdpr.jenkins.Launch "$1" "$2" "$3"
+echo $BIN
+java -classpath target/jenkins-reporting.jar:$BIN/conf com.disney.wdpr.jenkins.Launch "$1" "$2" "$3"
